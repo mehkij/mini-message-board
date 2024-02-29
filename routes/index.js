df8c3e23321002d33df2912d2c9b28099e67050c
Mehki Joseph <disismehki@gmail.com>
2024-02-29 08:36:06 -0500

    Move messages array out of router.get

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const messages = [
-    {
-      text: "Hi there!",
-      user: "Amando",
-      added: new Date(),
-    },
-    {
-      text: "Hello World!",
-      user: "Charles",
-      added: new Date(),
-    },
-  ];
-
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
