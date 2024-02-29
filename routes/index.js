0ee08d17441f23d1c5a8729589e972f58fc42315
Mehki Joseph <disismehki@gmail.com>
2024-02-29 08:41:33 -0500

    Add comments

--- routes/index.js
+++ routes/index.js
@@ -14,18 +14,21 @@ const messages = [
   },
 ];
 
-/* GET home page. */
+// GET home page.
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
+// GET form page
 router.get("/new", function (req, res, next) {
   res.render("form", { title: "New Message" });
 });
 
+// POST form
 router.post("/new", function (req, res, next) {
   const body = req.body;
   messages.push({ text: body.message, user: body.author, added: new Date() });
+
   res.redirect("/");
 });
 
